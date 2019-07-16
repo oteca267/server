93124dcbf39acb05181055ad06f6be36e484be97
CARLOS <oteca68@gmail.com>
2019-07-16 12:03:56 -0500

    mejorando servidor aunque hay un error ?

--- app.js
+++ app.js
@@ -1,8 +1,37 @@
 const express = require('express');
+const path =   require('path');
+const bodyparser = require('body-parser');
 const app = express();
+
+const routes = require( './routes/index.js');
 
 app.use(express.static('public'))
 
-app.listen(3000,function (){
-    console.log('server on port 3000')
+//sttings
+app.set('port',process.env.PORT || 3000 );
+app.set('views', path.join(__dirname, 'views') );
+app.set('view engine', 'ejs');
+
+
+// middlewares
+app.use((req, res, next) =>  {
+    console.log('${req.url} -${req.method}');
+    next();
+})
+
+app.use(body-Parser.join());
+app.use(body-Parser.urlenconded({extended: flase}));
+
+//routes
+
+app.use(routes);
+
+//static file
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+//start the server
+
+app.listen(app.get('port'), () => {
+    console.log('server on port ', app.get('port'))
 });
