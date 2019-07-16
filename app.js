f54a213523ca5ca9109464b25166daa1941b0820
Leonardo <lfotecam@gmail.com>
2019-07-15 21:20:31 -0500

    unsetr arrow functions

--- app.js
+++ app.js
@@ -3,4 +3,6 @@ const app = express();
 
 app.use(express.static('public'))
 
-app.listen(3000,() => console.log('server on port 3000'));
+app.listen(3000,function (){
+    console.log('server on port 3000')
+});
